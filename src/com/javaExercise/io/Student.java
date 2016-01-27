67e09ce4f0642fa52477ad2088987b3159339773
袁殷 <2212079150@qq.com>
2016-01-28 01:10:29 +0800

    序列化中的transient关键字,ArrayList源码使用transient对序列化进行优化

--- src/com/javaExercise/io/Student.java
+++ src/com/javaExercise/io/Student.java
@@ -1,14 +1,15 @@
 package com.javaExercise.io;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  * Created by yuanyin on 16/1/28.
  */
-public class Student implements Serializable{
+public class Student implements Serializable {
     private String stuNo;
     private String stuName;
-    private int stuSge;
+    private transient int stuSge;//该元素不会进行jvm默认的序列化,也可以自己完成这个元素的序列化
 
     public Student() {
     }
@@ -51,4 +52,35 @@ public class Student implements Serializable{
                 ", stuSge=" + stuSge +
                 '}';
     }
+
+    /**
+     * 自己完成元素的序列化,
+     * 参考ArrayList的writeObject方法
+     *
+     * @param s
+     * @throws java.io.IOException
+     */
+    private void writeObject(java.io.ObjectOutputStream s)
+            throws java.io.IOException {
+        s.defaultWriteObject();//把jvm能序列化的元素进行序列化操作
+        s.writeInt(stuSge);//自己完成stuSge的序列化
+    }
+
+    /**
+     * 反序列化 自己进行序列化的元素
+     *
+     * @param s
+     * @throws java.io.IOException
+     * @throws ClassNotFoundException
+     */
+    private void readObject(java.io.ObjectInputStream s)
+            throws java.io.IOException, ClassNotFoundException {
+        s.defaultReadObject();//把jvm默认能反序列化的元素进行反序列化
+        this.stuSge = s.readInt();//自己完成stuSge的反序列化
+    }
+
+    /**
+     * ArrayList源码使用transient进行序列化的优化,
+     * 只序列化有效元素,从而提高性能
+     */
 }
